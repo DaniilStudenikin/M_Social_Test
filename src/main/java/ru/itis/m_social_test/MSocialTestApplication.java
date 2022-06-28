1ce96cf095765e978125005b87806179d64cc387
Rapiera <fastrapier1@yandex.ru>
2022-06-28 12:43:10 +0300

    Initial commit

---
+++ src/main/java/ru/itis/m_social_test/MSocialTestApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.m_social_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSocialTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSocialTestApplication.class, args);
+    }
+
+}
